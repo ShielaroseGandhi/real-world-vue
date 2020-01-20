b9f8d74e525457d462abf7a031a81398f1c70313
Shielarose Gandhi <shielarose.gandhi@nofixedaddressinc.com>
2020-01-20 11:18:47 -0500

    deleted user component and added props to events

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,7 @@
     <div id="nav">
       <router-link :to="{ name: 'event-list' }">List</router-link> |
       <router-link :to="{ name: 'event-show' }">Show Event #1</router-link> |
-      <router-link :to="{ name: 'event-create' }">Create</router-link> |
-      <router-link :to="{ name: 'user', params: { username: 'Shielarose' } }"
-        >Shielarose</router-link
-      >
+      <router-link :to="{ name: 'event-create' }">Create</router-link>
     </div>
     <router-view />
   </div>
--- src/views/User.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>This is {{ $route.params.username }}'s page</h1>
-</template>
-
-<script>
-export default {
-  props: ['username']
-}
-</script>
