a7bb4d389b2080e972b521fe670abeeb57ed4c17
Shielarose Gandhi <shielarose.gandhi@nofixedaddressinc.com>
2020-01-20 11:12:55 -0500

    added user component and a dynamic route

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,10 @@
     <div id="nav">
       <router-link :to="{ name: 'event-list' }">List</router-link> |
       <router-link :to="{ name: 'event-show' }">Show Event #1</router-link> |
-      <router-link :to="{ name: 'event-create' }">Create</router-link>
+      <router-link :to="{ name: 'event-create' }">Create</router-link> |
+      <router-link :to="{ name: 'user', params: { username: 'Shielarose' } }"
+        >Shielarose</router-link
+      >
     </div>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import EventCreate from '../views/EventCreate.vue'
 import EventList from '../views/EventList.vue'
 import EventShow from '../views/EventShow.vue'
+import User from '../views/User.vue'
 
 Vue.use(VueRouter)
 
@@ -21,6 +22,12 @@ const routes = [
     path: '/event/create',
     name: 'event-create',
     component: EventCreate
+  },
+  {
+    path: '/user/:username',
+    name: 'user',
+    component: User,
+    props: true // sends our route parameters into our component as props
   }
 ]
 
