a3fbc88afeb6810cf37189e1ecd04e1363d32e1f
Shielarose Gandhi <shielarose.gandhi@nofixedaddressinc.com>
2020-01-20 11:57:09 -0500

    wrapped router link around event card component

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,9 +1,14 @@
 <template>
-  <div class="event-card -shadow">
-    <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
-    <h4 class="title">{{ event.title }}</h4>
-    <span>{{ event.attendees.length }}</span>
-  </div>
+  <router-link
+    class="event-link"
+    :to="{ name: 'event-show', params: { id: '1' } }"
+  >
+    <div class="event-card -shadow">
+      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+      <h4 class="title">{{ event.title }}</h4>
+      <span>{{ event.attendees.length }} attending</span>
+    </div>
+  </router-link>
 </template>
 
 <script>
@@ -14,7 +19,7 @@ export default {
         id: 1,
         title: 'Beach Cleanup',
         date: 'Tues Aug 19, 2018',
-        time: '6.00',
+        time: '6:00',
         attendees: [
           { id: 'abc123', name: 'Shielarose Gandhi' },
           { id: 'def456', name: 'Adam Jahr' }
