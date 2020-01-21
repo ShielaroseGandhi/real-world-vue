48d57ed397aa9aa192c33d4ff2b76b01cf997bab
Shielarose Gandhi <shielarose.gandhi@nofixedaddressinc.com>
2020-01-20 21:19:09 -0500

    Made Icon comp. and imported as a global component

--- src/main.js
+++ src/main.js
@@ -2,6 +2,34 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import BaseIcon from '@/components/BaseIcon.vue'
+import BaseButton from '@/components/BaseButton.vue'
+import BaseInput from '@/components/BaseInput.vue'
+
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+
+// Vue.component('BaseIcon', BaseIcon) // global component --> must be above new vue instance
+// Vue.component('BaseButton', BaseButton)
+// Vue.component('BaseInput', BaseInput)
+
+// another way to globally register components below
+const requireComponent = require.context(
+  './components', // directory to search within
+  false, // do we want to search sub directories?
+  /Base[A-Z]\w+\.(vue|js)$/ //search for components that match this regex
+)
+
+requireComponent.keys().forEach(fileName => {
+  //.keys gives array with each file
+  const componentConfig = requireComponent(fileName)
+
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+  ) // uses lodash methods to convert file names to pascal case
+
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
 
 Vue.config.productionTip = false
 
