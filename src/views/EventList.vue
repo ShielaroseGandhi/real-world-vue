c598eeee39f878d1c8ab48606f4c4423fd2cf3e1
Shielarose Gandhi <shielarose.gandhi@nofixedaddressinc.com>
2020-01-21 09:23:02 -0500

    performed axios api call

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,17 +1,34 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
     <BaseIcon />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import axios from 'axios'
 
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    axios
+      .get('http://localhost:3000/events')
+      .then(res => {
+        this.events = res.data
+        console.log(res.data)
+      })
+      .catch(error => {
+        console.log('There was an error:' + error.response)
+      })
   }
 }
 </script>
