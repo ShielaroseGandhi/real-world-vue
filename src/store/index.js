61c5b77a490a87ac1cc836de318791aa6527fb23
Shielarose Gandhi <shielarose.gandhi@nofixedaddressinc.com>
2020-01-24 16:03:57 -0500

    fetched list of events, added pagination, fetched single event with vuex

--- src/store/index.js
+++ src/store/index.js
@@ -16,11 +16,22 @@ export default new Vuex.Store({
       'food',
       'community'
     ],
-    events: []
+    events: [],
+    eventsTotal: 0,
+    event: {}
   },
   mutations: {
     ADD_EVENT(state, event) {
       state.events.push(event)
+    },
+    SET_EVENTS(state, events) {
+      state.events = events
+    },
+    SET_EVENTS_TOTAL(state, total) {
+      state.eventsTotal = total
+    },
+    SET_EVENT(state, event) {
+      state.event = event
     }
   },
   actions: {
@@ -28,6 +39,31 @@ export default new Vuex.Store({
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
       })
+    },
+    fetchEvents({ commit }, { perPage, page }) {
+      EventService.getEvents(perPage, page)
+        .then(res => {
+          commit('SET_EVENTS_TOTAL', res.headers['x-total-count'])
+          commit('SET_EVENTS', res.data)
+        })
+        .catch(error => {
+          console.log('There was an error:' + error.response)
+        })
+    },
+    fetchEvent({ commit, getters }, id) {
+      let event = getters.getEventById(id)
+
+      if (event) {
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(id)
+          .then(response => {
+            commit('SET_EVENT', response.data)
+          })
+          .catch(error => {
+            console.log('error:', error.response)
+          })
+      }
     }
   },
   getters: {
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,32 +2,43 @@
   <div>
     <h1>Events Listing</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <template v-if="page != 1">
+      <router-link
+        :to="{ name: 'event-list', query: { page: page - 1 } }"
+        rel="prev"
+        >Prev Page</router-link
+      >
+      |
+    </template>
+    <router-link
+      v-if="eventsTotal > page * 3"
+      :to="{ name: 'event-list', query: { page: page + 1 } }"
+      rel="next"
+      >Next Page</router-link
+    >
     <BaseIcon />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     EventCard
   },
-  data() {
-    return {
-      events: []
-    }
-  },
   created() {
-    EventService.getEvents()
-      .then(res => {
-        this.events = res.data
-        console.log(res.data)
-      })
-      .catch(error => {
-        console.log('There was an error:' + error.response)
-      })
+    this.$store.dispatch('fetchEvents', {
+      perPage: 3,
+      page: this.page
+    })
+  },
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    ...mapState(['events', 'eventsTotal'])
   }
 }
 </script>
